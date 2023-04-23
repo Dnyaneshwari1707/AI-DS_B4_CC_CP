b4a9c72dde1b8915f6e5a4346085627494566958
Navale Dnyaneshwari Vikas <130634010+Dnyaneshwari1707@users.noreply.github.com>
2023-04-23 13:55:05 +0530

    Update app.py

--- CC Cp/app.py
+++ CC Cp/app.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 # Define the route for the home page
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 # Define the route for the recommendation page
 @app.route('/recommend', methods=['POST'])
